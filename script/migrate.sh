97d3d907faa24e3e23cde471ef85f6e8997589bf
fahdjamy <fahdjamy2@gmail.com>
2025-03-10 12:40:41 -0500

    Change XRF_Q2_BID_PG_DB_URL env variable to XRF_BID_PG_DB_URL

--- script/migrate.sh
+++ script/migrate.sh
@@ -9,16 +9,15 @@ if ! command -v migrate &> /dev/null; then
   exit 1 # Exit with error code 1
 fi
 
-if [ -z "${XRF_Q2_BID_PG_DB_URL}" ]; then # -z to test if the length of a string is zero.
-  echo "Error: Environment variable XRF_Q2_BID_PG_DB_URL is not set."
-  echo "Please set this environment variable before running migrations."
+if [ -z "${XRF_BID_PG_DB_URL}" ]; then # -z to test if the length of a string is zero.
+  echo "Error: Environment variable XRF_BID_PG_DB_URL is not set."
   exit 1 # Exit with error code 1
 fi
 
 # Run go-migrate migrations
 echo "Running database migrations..."
 
-if migrate -database "${XRF_Q2_BID_PG_DB_URL}" -path ../db/migrations up; then
+if migrate -database "${XRF_BID_PG_DB_URL}" -path ../db/migrations up; then
   echo "Database migrations completed successfully."
 else
   echo "Error: Database migrations failed. Please check the logs and your go-migrate setup."
