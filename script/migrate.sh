cd62bf20b8576e3f544c052d51401d76956a3686
fahdjamy <fahdjamy2@gmail.com>
2025-03-10 10:38:16 -0500

    Add check that go-migrate is installed in the path check in the shell script

--- script/migrate.sh
+++ script/migrate.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Check if the 'migrate' command is installed
+if ! command -v migrate &> /dev/null; then
+  # command -v migrate: command checks if the command migrate is in the PATH and is executable
+  # -v option for command makes it print the full path of the command migrate if found
+  echo "Error: 'migrate' command not found. Please install golang-migrate: follow instructions here:"
+  echo "https://github.com/golang-migrate/migrate/tree/master/cmd/migrate"
+  exit 1 # Exit with error code 1
+fi
+
 if [ -z "${XRF_Q2_BID_PG_DB_URL}" ]; then # -z to test if the length of a string is zero.
   echo "Error: Environment variable XRF_Q2_BID_PG_DB_URL is not set."
   echo "Please set this environment variable before running migrations."
