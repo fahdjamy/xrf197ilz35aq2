349dac49a4e853821fbe047fe275ba298f5b782e
fahdjamy <fahdjamy2@gmail.com>
2025-03-11 14:55:46 -0500

    Refactor sql code

--- storage/migrations/000001_create_session_table.up.sql
+++ storage/migrations/000001_create_session_table.up.sql
@@ -17,33 +17,17 @@ CREATE TYPE session_status AS ENUM (
 
 CREATE TABLE IF NOT EXISTS bid_session
 (
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    auto_execute
-    BOOLEAN
-    NOT
-    NULL,
-    user_fp
-    VARCHAR
-(
-    255
-) NOT NULL,
-    asset_id VARCHAR
-(
-    255
-) NOT NULL,
+    id                   SERIAL PRIMARY KEY,
+    auto_execute         BOOLEAN          NOT NULL,
+    user_fp              VARCHAR(255)     NOT NULL,
+    asset_id             VARCHAR(255)     NOT NULL,
     status session_status NOT NULL,
-    session_name VARCHAR
-(
-    100
-) NOT NULL,
+    session_name         VARCHAR(100)     NOT NULL,
     reserve_price DOUBLE PRECISION NOT NULL,
     auction_type session_action_type NOT NULL,
     end_time TIMESTAMP WITH TIME ZONE NOT NULL,
     start_time TIMESTAMP WITH TIME ZONE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                             current_highest_bid DOUBLE PRECISION NOT NULL,
-                             bid_increment_amount DOUBLE PRECISION NOT NULL
-                             );
+    current_highest_bid  DOUBLE PRECISION NOT NULL,
+    bid_increment_amount DOUBLE PRECISION NOT NULL
+);
