0176eac67b4726737d6f3c5cbd988ec13051de14
fahdjamy <fahdjamy2@gmail.com>
2025-03-16 17:05:55 -0500

    Add create bid schema migrations

---
+++ storage/migrations/000002_create_bid_table.up.sql
@@ -0,0 +1,18 @@
+-- Create the ENUM for BidStatus
+CREATE TYPE bid_status AS ENUM (
+    'PENDING',
+    'REJECTED',
+    'ACCEPTED'
+);
+
+CREATE TABLE IF NOT EXISTS asset_bid (
+    id SERIAL PRIMARY KEY,
+    accepted BOOLEAN NOT NULL,
+    status bid_status NOT NULL,
+    asset_id VARCHAR(255) NOT NULL,
+    amount DOUBLE PRECISION NOT NULL,
+    placed_by VARCHAR(255) NOT NULL,
+    session_id VARCHAR(255) NOT NULL,
+    lastUntil TIMESTAMP WITH TIME ZONE NOT NULL,
+    placed_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
