0785318c082bcf9b61e58d559f128d9841dbae6e
fahdjamy <fahdjamy2@gmail.com>
2025-05-20 18:37:32 -0500

    Refactor, create a bid proto message and response and add GetUserBid and StreamOpenBids response

--- internal/exchange/bid.go
+++ internal/exchange/bid.go
@@ -4,9 +4,7 @@ import "time"
 
 type BidRequest struct {
 	Amount    float64   `json:"amount"`
-	Status    string    `json:"status"`
 	UserFp    string    `json:"placedBy"`
-	Placed    time.Time `json:"placedAt"`
 	AssetId   string    `json:"assetId"`
 	LastUntil time.Time `json:"lastUntil"`
 }
